16c31816ec0a90c5715b6f6101eeeb10026155f6
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-08 14:34:46 -0700

    Made view for submitting post request and receive the post request server side

--- express_server.js
+++ express_server.js
@@ -3,7 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {
+  let alphNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  let result = '';
+  for (let i = 0; i < 6; i++){
+    let j = Math.floor(Math.random()*(alphNum.length-1));
+    result += alphNum[j];
+  }
+  return result;
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,11 +35,21 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  //const templateVars = { longURL: req.body.longURL };
+  //urlDatabase[generateRandomString()] = longURL;
+  console.log(req.body);
+  res.send("ok");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
